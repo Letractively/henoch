535f269dcdb02a843766c005d69ab6a1985c7fbc
uts.dmodiwirijo <uts.dmodiwirijo@c1c56043-084b-9978-06b2-3c74b1ed613e>
2011-01-22 21:43:13 +0000

    delegates added

--- Telerik/Crapyard/genesis/parallelresourcer/Program.cs
+++ Telerik/Crapyard/genesis/parallelresourcer/Program.cs
@@ -11,9 +11,14 @@ namespace ParallelResourcer
         static void Main(string[] args)
         {
             Tree<string> taken = CreateTasks();
+            var treeHandler = new Tree<string>.TreeHandler(OnTreeEvent);
+            taken.RegisterWithTree(treeHandler);
+            var myTask = new Tree<string>.TreeHandler(MyTask);
+            taken.RegisterWithTree(myTask);
+
             var t1 = Task.Factory.StartNew(() =>
             {
-                for (int i = 0; i < 1000; i++)
+                for (int i = 0; i < 1; i++)
                 {
                    
                     Tree<string>.WalkParallel(taken,
@@ -22,10 +27,16 @@ namespace ParallelResourcer
             });
 
             Task.WaitAll(t1);
+
+            taken.UnRegisterWithTree(treeHandler);
+            taken.UnRegisterWithTree(myTask);
+            Console.ReadKey();
         }
         private static Tree<string> CreateTasks()
         {
             Tree<string> taken = new Tree<string>();
+
+
             taken.Left = new Tree<string>();
             taken.Right = new Tree<string>();
             taken.Data = "root";
@@ -36,11 +47,20 @@ namespace ParallelResourcer
             taken.Left.Left.Data = "C-" + taken.Left.Data;
             taken.Left.Right = new Tree<string>();
             taken.Left.Right.Data = "D-" + taken.Left.Data;
+
+
             return taken;
         }
         private static void MyTask(string woord)
         {
             Console.WriteLine(woord);
+        }
+        // This is the target for incoming events.
+        public static void OnTreeEvent(string msg)
+        {
+            Console.WriteLine("\n***** Message From Tree root *****");
+            Console.WriteLine("=> {0}", msg);
+            Console.WriteLine("***********************************\n");
         }
     }
 }
