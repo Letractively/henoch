6632565ccb7658f943807d3e14c5fa114e32a659
uts.dmodiwirijo <uts.dmodiwirijo@c1c56043-084b-9978-06b2-3c74b1ed613e>
2011-01-25 22:30:01 +0000

    async completed

--- trunk/Telerik/Crapyard/TelerikExample/AsyncViewer.aspx.cs
+++ trunk/Telerik/Crapyard/TelerikExample/AsyncViewer.aspx.cs
@@ -15,18 +15,24 @@ namespace TelerikExample
         private WebRequest m_MyRequest;
 
         protected void Page_Load(object sender, EventArgs e)
-        {
-            //Timer1.Enabled = true;
-            Label1.Text = "BeginProcessRequest starting ...";
+        {                      
+            if (!IsPostBack && IsAsync)
+            {
+                Label1.Text = "BeginProcessRequest starting ...";
+                Timer1.Enabled = true;
+                AddOnPreRenderCompleteAsync(
+                    new BeginEventHandler(BeginProcessRequest),
+                    new EndEventHandler(EndProcessRequest));
 
-            AddOnPreRenderCompleteAsync(
-                new BeginEventHandler(BeginProcessRequest),
-                new EndEventHandler(EndProcessRequest));
+                // Initialize the WebRequest.                  
+                string address = "http://localhost/";
+                ;// Request.Url.ToString();                    
+                m_MyRequest = System.Net.WebRequest.Create(address);
+            }
+            else
+            {
 
-            // Initialize the WebRequest.                  
-            string address = "http://localhost/";
-            ;// Request.Url.ToString();                    
-            m_MyRequest = System.Net.WebRequest.Create(address);  
+            }
         }
         public bool IsReusable
         {
@@ -36,11 +42,11 @@ namespace TelerikExample
 
         public IAsyncResult BeginProcessRequest(object sender, EventArgs eventArgs, AsyncCallback cb, object extraData)
         {
-            Label1.Text = "BeginGetAsyncData: thread #" + Thread.CurrentThread.GetHashCode();
+            Session["AsyncIsCompleted"] = null;
+            Label1.Text = "BeginGetAsyncData: thread #" + Thread.CurrentThread.ManagedThreadId;
             Trace.Write("BeginGetAsyncData", Label2.Text); 
             //Response.Write("<p>BeginProcessRequest starting ...</p>");
             operationPattern = new AsynchOperationPattern(CallBackResult, this.Context, extraData);
-           
             operationPattern.StartAsync();
             
             Session["label3"] = Label3;
@@ -50,16 +56,9 @@ namespace TelerikExample
             return m_MyRequest.BeginGetResponse(cb, Session["label3"]); 
         }
 
-        private void DoAsync(AsyncCallback cb, object extraData)
-        {
-
-        }
-
         public void EndProcessRequest(IAsyncResult result)
-        {
-            
-            int threadId = Thread.CurrentThread.GetHashCode();              
-            //int threadId = Thread.CurrentThread.ManagedThreadId;              
+        {                        
+            int threadId = Thread.CurrentThread.ManagedThreadId;
             Label2.Text = "EndGetAsyncData: thread #" + threadId;              
             Trace.Write("EndGetAsyncData", Label3.Text);                
             //System.Net.WebResponse myResponse = myRequest.EndGetResponse(ar);               
@@ -77,32 +76,17 @@ namespace TelerikExample
         {
             try
             {
-                if (operationPattern != null)
-                {
-                    Session["label3"] = operationPattern.AsyncState;
-                    Label3.Text = Session["label3"] as string;
-                }
-                
+                Label3.Text = Session["label3"] as string;
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
 
-            if (operationPattern!=null && operationPattern.IsCompleted)
+            if (Session["AsyncIsCompleted"]!=null)
                 Timer1.Enabled = false;
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-
-            Timer1.Enabled = true;
-            AsyncCallback cb = new AsyncCallback(CallBackResult);
-            object extraData = Session["label3"];
-            DoAsync(cb, extraData);
-           
-            
-        }
         private void CallBackResult(IAsyncResult result)
         {
             int threadId;
@@ -110,17 +94,21 @@ namespace TelerikExample
 
             //If ac is a delegate: AsynchOperationPattern ac = (AsynchOperationPattern)((AsyncResult)result).AsyncDelegate;
             var res = result;
-            Session["label3"] = operationPattern.AsyncState;
-            Label3.Text = operationPattern.AsyncState as string;
+            
             //result.AsyncWaitHandle.Close();     
             if (operationPattern.IsCompleted)
             {
                 Timer1.Enabled = false;
-                if (operationPattern.MyThread.IsAlive)
-                {
-                    operationPattern.MyThread.Abort();
-                }
+
+                Session["AsyncIsCompleted"] = "AsyncIsCompleted";
+               
             }
+            Session["label3"] = operationPattern.AsyncState;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
