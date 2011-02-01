a1322338dcb7cd52494995d6d86721afd13ec3d8
uts.dmodiwirijo <uts.dmodiwirijo@gmail.com>
2011-02-01 12:28:36 +0000

    request state added

---
+++ Telerik/Crapyard/Observlet/Observlet/WebForms/RequestState.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Observlet.WebForms
+{
+    public class RequestState
+    {
+        // This class stores the State of the request.
+        public static readonly int BUFFER_SIZE = 1024;
+        public StringBuilder requestData;
+        public byte[] BufferRead;
+        public HttpWebRequest request;
+        public HttpWebResponse response;
+        public Stream streamResponse;
+        public RequestState()
+        {
+            BufferRead = new byte[BUFFER_SIZE];
+            requestData = new StringBuilder("");
+            request = null;
+            streamResponse = null;
+        }
+    }
+}
