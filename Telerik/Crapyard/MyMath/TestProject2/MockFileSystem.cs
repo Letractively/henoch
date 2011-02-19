fa6bd7b31d0b0f3038db7782a7e9c0c7ce467e92
uts.dmodiwirijo <uts.dmodiwirijo@gmail.com>
2011-02-19 12:57:38 +0000

    moles pex updated

---
+++ Telerik/Crapyard/MyMath/TestProject2/MockFileSystem.cs
@@ -0,0 +1,14 @@
+﻿using MyMath;
+
+namespace TestProject2
+{
+    public class MockFileSystem : IFileSystem
+    {
+        public string Content;
+
+        public string ReadAllText(string fileName)
+        {
+            return this.Content;
+        }
+    }
+}
---
+++ Telerik/Crapyard/MyMath/TestProject2/TestReaderWithStubs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MyMath;
+
+namespace TestProject2
+{
+    public class TestReaderWithStubs
+    {
+        private IFileSystem fs;
+        //constructor 
+        public TestReaderWithStubs(IFileSystem fs)
+        {
+            this.fs = fs;
+        }
+
+        public void LoadFile(string fileName)
+        {
+            var content = this.fs.ReadAllText(fileName);
+            if (!content.StartsWith("test")) throw new ArgumentException("invalid file");
+            this.Content = content;
+        }
+
+        public string Content { get; private set; }
+    }
+}
