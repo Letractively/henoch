ecbc7e839489e8103dc998dafbaeefea4b17c7e4
uts.dmodiwirijo@gmail.com <uts.dmodiwirijo@gmail.com@c1c56043-084b-9978-06b2-3c74b1ed613e>
2011-02-19 12:57:38 +0000

    moles pex updated

---
+++ trunk/Telerik/Crapyard/MyMath/TestProject2/TestReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyMath;
+
+namespace TestProject2
+{
+    public class TestReader
+    {
+        private IFileSystem fs;
+
+        public string Content { get; private set; }
+
+        internal void LoadFile(string fileName)
+        {
+            var content = FileSystem.ReadAllText(fileName);
+            if (!content.StartsWith("test")) throw new ArgumentException("invalid file");
+            this.Content = content;
+        }
+    }
+}
---
+++ trunk/Telerik/Crapyard/MyMath/TestProject2/UnitTest1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Moles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMath;
+using MyMath.Moles;
+
+namespace TestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [HostType("Moles")]
+        public void Y2KCheckerTest()
+        {
+            //arrange
+            MDateTime.NowGet = () => new DateTime(2000, 1, 1);
+            //act
+            FileSystem.Check();
+
+            //assert
+
+        }
+        [TestMethod]
+        public void CheckValidFile()
+        {
+            // arrange 
+            var fileName = "test.txt";
+            var content = "test";
+            File.WriteAllText(fileName, content);
+            
+            // act 
+            var test = new TestReader();
+            test.LoadFile(fileName);
+
+            // assert
+            Assert.AreEqual(content, test.Content);
+        }
+
+        [TestMethod]
+        public void CheckValidFile2()
+        {
+            // arrange 
+            string fileName = "test.txt";
+            var content = "test";
+            var fs = new MockFileSystem();
+            fs.Content = content;
+
+            // act 
+            var test = new TestReader();
+            test.LoadFile(fileName);
+
+            // assert 
+            Assert.AreEqual(content, test.Content);
+        }
+
+        [TestMethod]
+        public void CheckValidFileWithStubs()
+        {
+            // arrange 
+            var fileName = "test.txt";
+            var content = "test";
+            //File.WriteAllText(fileName, content); 
+            var fs = new SIFileSystem();
+            fs.ReadAllTextString = delegate(string f)
+                                       {
+                                           Assert.IsTrue(f == fileName);
+                                           return content;
+                                       };
+
+            // act 
+            //var test = new TestReader(); 
+            var test = new TestReaderWithStubs(fs);
+            test.LoadFile(fileName);
+
+            // assert 
+            Assert.AreEqual(content, test.Content);
+        }
+
+        [TestMethod]
+        [HostType("Moles")]
+        public void CheckValidFileWithMoles()
+        {
+            // arrange 
+            var fileName = "test.txt";
+            var content = "test";
+            MFileSystem.ReadAllTextString = (string f) =>
+                                                {
+                                                    Assert.IsTrue(f == fileName);
+                                                    return content;
+                                                };
+
+            // act 
+            var test = new TestReader();
+            test.LoadFile(fileName);
+
+            // assert 
+            Assert.AreEqual(content, test.Content);
+        }
+    }
+}
