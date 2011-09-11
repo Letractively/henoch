d935fa1edde01c15e4c41c2a64d88baf526ff2ce
uts.dmodiwirijo <uts.dmodiwirijo@gmail.com>
2011-09-11 19:09:17 +0000

    ordermanagement

---
+++ Crappacino/OrderManagement/Source/Infrastructure/Infrastructure.Layout/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Infrastructure.Layout")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Infrastructure.Layout")]
+[assembly: AssemblyCopyright("Copyright © ")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7bbfc7e1-7d52-4108-b39b-947739763b07")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
