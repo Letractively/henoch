75c3efb04efdf4e43840d4207f4cde273ec35373
aldrin.ismail@gmail.com <aldrin.ismail@gmail.com@c1c56043-084b-9978-06b2-3c74b1ed613e>
2012-05-20 19:52:31 +0000

    versiebeheer naar 2012 op 20 mei

--- trunk/Telerik/Crapyard/TelerikExample/Properties/AssemblyInfo.cs
+++ trunk/Telerik/Crapyard/TelerikExample/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2012.2.2005.35")]
+[assembly: AssemblyFileVersion("2012.2.2005.35")]
