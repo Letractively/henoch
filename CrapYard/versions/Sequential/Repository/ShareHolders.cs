db9a93d72bbcef38e7eb152b155fd68537b7d245
sneakyboo <sneakyboo@gmail.com>
2012-09-19 14:39:30 +0000

    XML ok. (sequential solution)

---
+++ CrapYard/versions/Sequential/Repository/ShareHolders.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caching;
+using Microsoft.Practices.EnterpriseLibrary.Caching;
+using ParallelResourcer;
+
+namespace Repository
+{
+    /// <summary>
+    /// Focusses on shareholders and their relations. The data is cached.
+    /// </summary>
+    public class ShareHolders
+    {
+        private const string cShareHolder ="shareholders";
+        /// <summary>
+        /// linked list of parents and their children
+        /// </summary>
+        IDictionary<string, string> _organiGraph = new Dictionary<string, string>();
+        /// <summary>
+        /// load the cache from dataresources involved
+        /// </summary>
+        public ShareHolders()
+        {
+            InitializeCache();
+        }
+
+        /// <summary>
+        /// linked list of parents and their children. Companies are stored in cache. 
+        /// </summary>
+        /// <returns></returns>
+        public IDictionary<string, IList<string>> Companies
+        {
+            get
+            {
+                IDictionary<string, IList<string>> list = InitializeCache();
+
+                return list;
+            }
+        }
+        /// <summary>
+        /// See http://xlinux.nist.gov/dads//HTML/dictionary.html
+        /// </summary>
+        /// <returns></returns>
+        private static IDictionary<string, IList<string>> InitializeCache()
+        {
+            IDictionary<string, IList<string>> dictionary = null;
+            var myRepository = MyCache<Object>.CacheManager;
+            if (myRepository != null)
+            {
+                dictionary = myRepository.GetData(cShareHolder) as IDictionary<string, IList<string>>;
+                if (dictionary == null)
+                {
+                    //Initialize
+                    dictionary = new Dictionary<string, IList<string>>();
+                    myRepository.Add(cShareHolder, dictionary);
+                }
+            }
+            return dictionary;
+        }
+        public IList<string> GetSubsidiaries(string shareHolder)
+        {
+            IList<string> list =Tree<string>.GetChildren(shareHolder, Companies);
+            return list;
+
+        }
+        public IList<string> GetShareHolders(string subsidiary)
+        {
+            var listShareHolders =Tree<string>.GetParents(subsidiary, Companies);
+
+            return listShareHolders.ToList<string>();
+        }
+
+        /// <summary>
+        /// adds a new shareholder with empty list of subsidiaries.
+        /// </summary>
+        /// <param name="shareHolder"></param>
+        public void AddShareHolders(string shareHolder)
+        {
+            Companies.Add(shareHolder, new List<string>());
+        }
+        public void AddSubsidiary(string shareHolder, string subsidiary)
+        {
+            Companies[shareHolder].Add(subsidiary);
+        }
+        public void RemoveSubsidiary(string shareHolder, string subsidiary)
+        {
+            Companies[shareHolder].Remove(subsidiary);
+
+        }
+        /// <summary>
+        /// Reloads cache from dataresources.
+        /// </summary>
+        public void Refresh()
+        {
+            Companies.Clear();
+            string shareHolder = "Ahold";
+            //_listCompanies.Add(shareHolder, new List<string>());
+            //_listCompanies[shareHolder].Add("123");
+            //_listCompanies[shareHolder].Add("ah567");
+            this.AddShareHolders("Ahold");
+            this.AddSubsidiary("Ahold", "123");
+            this.AddSubsidiary("Ahold", "ah567");
+
+            this.AddShareHolders("Unilever");
+            this.AddSubsidiary("Unilever", "123");
+            this.AddSubsidiary("Unilever", "u567");
+
+            this.AddShareHolders("Shell");
+            this.AddSubsidiary("Shell", "s123");
+            this.AddSubsidiary("Shell", "s567");
+
+            this.AddShareHolders("123");
+            this.AddSubsidiary("123", "c123");
+            this.AddSubsidiary("123", "c123");
+        }
+        public Tree<string> GetRoot(string company)
+        {
+           Tree<string> NTree = new Tree<string>();
+
+            
+
+            
+            return NTree;
+        }
+
+        private void GetAncestors(string candidate)
+        {
+            IList<string> listAncestors = GetShareHolders(candidate);
+            foreach (var item in listAncestors)
+            {
+                
+            }
+
+            if (listAncestors != null && listAncestors[0].Equals(candidate))
+            {
+                listAncestors.Add(candidate);
+            }
+            else
+            {
+
+                listAncestors.Add(candidate);
+            }
+
+            ;
+        }
+
+    }
+}
---
+++ CrapYard/versions/Sequential/WebApplication1/TrotterOverview.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+using System.Collections.Generic;
+
+public partial class TrotterOverview : System.Web.UI.Page 
+{
+    private string gridMessage = null;
+
+    public string OrderNo
+    {
+        get { return Session["OrdNo"] as string; }
+        set { Session["OrdNo"] = value; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack)
+        {
+            OrderNo = "O201104322";
+        }
+
+    }
+    protected void RadGrid1_DataBound(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrEmpty(gridMessage))
+        {
+            DisplayMessage(gridMessage);
+        }
+    }
+
+	protected void RadGrid1_ItemUpdated(object source, Telerik.Web.UI.GridUpdatedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            e.KeepInEditMode = true;
+            e.ExceptionHandled = true;
+            SetMessage("Update failed. Reason: " + e.Exception.Message);
+        }
+        else
+        {
+            SetMessage("Item updated!");
+        }
+    }
+
+    protected void RadGrid1_ItemInserted(object source, GridInsertedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            e.ExceptionHandled = true;
+            SetMessage("Insert failed! Reason: " + e.Exception.Message);
+        }
+        else
+        {
+            SetMessage("New product is inserted!");
+        }
+    }
+
+    protected void RadGrid1_ItemDeleted(object source, GridDeletedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            e.ExceptionHandled = true;
+            SetMessage("Delete failed! Reason: " + e.Exception.Message);
+        }
+        else
+        {
+            SetMessage("Item deleted!");
+        }
+    }
+
+	private void DisplayMessage(string text)
+    {
+        RadGrid1.Controls.Add(new LiteralControl(string.Format("<span style='color:red'>{0}</span>", text)));
+    }
+
+    private void SetMessage(string message)
+    {
+        gridMessage = message;
+    }
+    //void GridTableView_Load(object sender, EventArgs e)
+    //{
+    //    GridTableView table = (GridTableView)sender;
+    //    foreach (GridDataItem item in table.Items)
+    //    {
+    //        if (item.Expanded)
+    //        {
+    //            //build the item key that will be used to save the item as expanded
+    //            string itemKey = BuildItemKey(item);
+    //            if (!ExpandedItemKeys.Contains(itemKey))
+    //            {
+    //                ExpandedItemKeys.Add(itemKey);
+    //            }
+    //        }
+    //    }
+    //}
+
+    //void GridTableView_DataBound(object sender, EventArgs e)
+    //{
+    //    GridTableView table = (GridTableView)sender;
+
+    //    foreach (GridDataItem item in table.Items)
+    //    {
+    //        string itemKey = BuildItemKey(item);
+    //        //if the item key is contained in the collection of saved items
+    //        //this means the item should be expanded
+    //        if (!item.Expanded && ExpandedItemKeys.Contains(itemKey))
+    //        {
+    //            item.Expanded = true;
+    //            ExpandedItemKeys.Remove(itemKey);
+    //        }
+    //    }
+
+    //}
+
+    ///// <summary>
+    ///// Build an item key that will uniquely identify a grid item
+    ///// among all the items in the RadGrid hierarchy. Use a combination 
+    ///// of the OwnerTableView.UniqueID and the set of all data values.
+    ///// </summary>
+    //protected string BuildItemKey(GridDataItem item)
+    //{
+    //    string[] keyNames = item.OwnerTableView.DataKeyNames;
+    //    if (keyNames.Length == 0) return item.ItemIndexHierarchical;
+
+    //    string returnKey = item.OwnerTableView.UniqueID + "::";
+
+    //    foreach (string keyName in keyNames)
+    //    {
+    //        string val = item.GetDataKeyValue(keyName).ToString();
+    //        OrderNo = val;
+    //        returnKey += val;
+
+    //        if (keyName != keyNames[keyNames.Length - 1])
+    //        {
+    //            returnKey += "::";
+    //        }
+    //    }
+
+    //    return returnKey;
+    //}
+
+    ///// <summary>
+    ///// Gets the expanded item state saved in the Session
+    ///// </summary>
+    //protected List<string> ExpandedItemKeys
+    //{
+    //    get
+    //    {
+    //        if (Session["ExpandedItemKeys"] == null)
+    //        {
+    //            Session["ExpandedItemKeys"] = new List<string>();
+    //        }
+
+    //        return (List<string>)Session["ExpandedItemKeys"];
+    //    }
+    //}
+
+    protected void RadGrid1_DetailTableDataBind(object sender, GridDetailTableDataBindEventArgs e)
+    {
+        OrderNo = (string)e.DetailTableView.ParentItem.GetDataKeyValue("ordNo");
+        e.DetailTableView.DataSource = SqlDataSource1;
+    }
+
+    protected void RadGrid1_NeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+    {
+
+    }
+}
