068e1694df0472bc7385291f6e48ade1785811a2
sneakyboo@gmail.com <sneakyboo@gmail.com@c1c56043-084b-9978-06b2-3c74b1ed613e>
2012-09-23 16:47:46 +0000

    sp's added

---
+++ trunk/CrapYard/WebApplication1/Repository/Sql/GetInstalledTrotters.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE GetInstalledTrotters
+AS
+
+
+	SELECT distinct proj.* FROM
+ 	troProject proj INNER JOIN
+	( 
+     SELECT pjtcd
+     FROM troProject_Act
+     WHERE  st = 'D' AND DATEDIFF(day, datefr, GETDATE())  > 30  AND NOT (DATEDIFF(day, dateTo, GETDATE())  > 0)
+  	) pact   
+
+ON proj.pjtcd = pact.pjtcd   
+ 
+ORDER BY dateFR desc
+RETURN 
